f968d6ee096711f4b255e424d09fbdbd1b0f0eb3
ajiswn <ajiisetiawann468@gmail.com>
2024-12-09 03:44:27 +0700

    Fix Some Warning

--- app/src/main/java/com/fruitastic/ui/main/MainViewModel.kt
+++ app/src/main/java/com/fruitastic/ui/main/MainViewModel.kt
@@ -3,10 +3,8 @@ package com.fruitastic.ui.main
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
-import androidx.lifecycle.viewModelScope
 import com.fruitastic.data.Repository
 import com.fruitastic.data.pref.UserModel
-import kotlinx.coroutines.launch
 
 class MainViewModel(private val repository: Repository) : ViewModel() {
 
